5c4111e036c0ddad1d5104c3690e5ffdaf495a95
rolandkal <rolandka@live.com>
2015-07-29 15:29:42 +0100

    JSON object support

--- src/MailChimp.php
+++ src/MailChimp.php
@@ -66,7 +66,7 @@ class MailChimp
 
         $url = $this->api_endpoint.'/'.$method;
 
-        $json_data = json_encode($args);
+        $json_data = json_encode($args, JSON_FORCE_OBJECT);
 
         if (function_exists('curl_init') && function_exists('curl_setopt')) {
             $ch = curl_init();
