059792f1f1a467834397ead38f245bdfcd09fc4a
drewm <drew.mclellan@gmail.com>
2018-02-16 16:06:26 +0000

    I can’t even

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -1,12 +1,7 @@
-#!/usr/bin/env bash
-
-# e causes to exit when one commands returns non-zero
-# v prints every line before executing
-set -ev
+#!/bin/bash
 
 cd ${TRAVIS_BUILD_DIR}
 
-if [[ $TRAVIS_PHP_VERSION = '7.1' ]]
-then
+if [[ $TRAVIS_PHP_VERSION = '7.1' ]]; then
     phpunit
 fi
