364ac2834915aa0183143b989f0195d2ed008878
Patrick Breen <pdbreen@gmail.com>
2018-02-28 06:41:06 -0500

    Add Allow header to PUT requests

--- src/MailChimp.php
+++ src/MailChimp.php
@@ -227,6 +227,10 @@ class MailChimp
             $httpHeader[] = "Accept-Language: " . $args["language"];
         }
 
+        if ($http_verb === 'put') {
+            $httpHeader[] = 'Allow: PUT, PATCH, POST';
+        }
+
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_HTTPHEADER, $httpHeader);
